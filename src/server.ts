f19bff1ba7a7a53998d05cca89d3a4cf9d8645f0
KAMAH <limitlesskds@gmail.com>
2023-04-07 22:57:29 +0100

    .

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors()); // add more security checks to cors 
+
+
+// Add error handler 
+
+
+// register routes
+
+const PORT = process.env.PORT || 3000;  // use node_env
+
+app.listen(PORT, function start() {
+	console.log(`Server Listening for connections on port ${PORT}`);
+});
