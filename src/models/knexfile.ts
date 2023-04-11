2d4283db0dc0eae60e567b19466f52208857f2ff
KAMAH <limitlesskds@gmail.com>
2023-04-11 21:45:43 +0100

    Ran lint command in codebase

--- src/models/knexfile.ts
+++ src/models/knexfile.ts
@@ -1,56 +1,54 @@
-import type { Knex } from "knex";
-
 // Update with your config settings.
 
 module.exports  = {
-  development: {
-    client: "pg",
-    connection: {
-      host: '0.0.0.0', //docker host
-      port: 15432,   // docker host port
-      user: 'rms',
-      password: "5E^&()VB78954!@PG"
-    },
-    pool: {
-      min: 0,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
+	development: {
+		client: 'pg',
+		connection: {
+			host: '0.0.0.0', //docker host
+			port: 15432,   // docker host port
+			user: 'rms',
+			password: '5E^&()VB78954!@PG'
+		},
+		pool: {
+			min: 0,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
 
-  // staging: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // },
+	// staging: {
+	//   client: "postgresql",
+	//   connection: {
+	//     database: "my_db",
+	//     user: "username",
+	//     password: "password"
+	//   },
+	//   pool: {
+	//     min: 2,
+	//     max: 10
+	//   },
+	//   migrations: {
+	//     tableName: "knex_migrations"
+	//   }
+	// },
 // 
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
+	// production: {
+	//   client: "postgresql",
+	//   connection: {
+	//     database: "my_db",
+	//     user: "username",
+	//     password: "password"
+	//   },
+	//   pool: {
+	//     min: 2,
+	//     max: 10
+	//   },
+	//   migrations: {
+	//     tableName: "knex_migrations"
+	//   }
+	// }
 // 
 };
 
--- src/models/migrations/20230410192746_test.ts
+++ src/models/migrations/20230410192746_test.ts
@@ -1,17 +1,15 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable("test", function(table){
-        table.increments('id')
-        table.string('first_name', 60).notNullable();
-        table.integer('price', 5).nullable()
-    })
+	return knex.schema.createTable('test', function(table){
+		table.increments('id');
+		table.string('first_name', 60).notNullable();
+		table.integer('price', 5).nullable();
+	});
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable("test")
+	return knex.schema.dropTable('test');
 }
-
-exports.config ={transaction: true}
