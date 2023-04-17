1b72992bc085b99ca746e8c63ecffbe8a39c9af0
KAMAH <limitlesskds@gmail.com>
2023-04-17 12:28:38 +0100

    Bug fix in this merge -> Revert "Merge pull request #12 from Uppist/runtimeTypeCheck"
    This reverts commit 4fe8dc1bad7271b73f1dbf1cc0ae0fa95dd93475, reversing
    changes made to e6289a0e31a0f08413fd0d6aef047203d520b49e.

--- src/controllers/userCTRL.ts
+++ src/controllers/userCTRL.ts
@@ -5,33 +5,14 @@ import { IUser} from '../models/users';
 
 import UserService from '../services/UserService';
 
-
 export default class UserControllers {
 
   async create (req:IReq<IUser>, res: IRes) {
     const user:IUser = req.body;
-    console.log (user)
-    if (instanceOfUser(user)){
       await UserService.createUser(user);
-      res.status(HttpStatusCodes.OK).json({user});
-    }
-    res.status(HttpStatusCodes.BAD_REQUEST).json(AppConstants.BAD_INPUT_FILED)
-  }
+		return res.status(HttpStatusCodes.OK).json({user});
+	}
 }
 
-/**
- * Check if incoming request is a type of IUser
- * @param object object to check against the interface
- * @returns boolean
- */
-function instanceOfUser(object: any): object is IUser {
-  return (
-    'email' && 
-    'full_name' && 
-    'role' && 
-    'password' && 
-    'phone_number'  in object
-  )
-}
 // beware ts is not typesafe at runtime perfom some valiation
 
