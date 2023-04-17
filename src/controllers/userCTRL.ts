6c1ee4872a06bd7da73a1b076e0f4fed5d8ae315
KAMAH <limitlesskds@gmail.com>
2023-04-17 09:12:13 +0100

    Created function to check for runtime type check for the user module

--- src/controllers/userCTRL.ts
+++ src/controllers/userCTRL.ts
@@ -5,14 +5,33 @@ import { IUser} from '../models/users';
 
 import UserService from '../services/UserService';
 
+
 export default class UserControllers {
 
   async create (req:IReq<IUser>, res: IRes) {
     const user:IUser = req.body;
+    console.log (user)
+    if (instanceOfUser(user)){
       await UserService.createUser(user);
-		return res.status(HttpStatusCodes.OK).json({user});
-	}
+      res.status(HttpStatusCodes.OK).json({user});
+    }
+    res.status(HttpStatusCodes.BAD_REQUEST).json(AppConstants.BAD_INPUT_FILED)
+  }
 }
 
+/**
+ * Check if incoming request is a type of IUser
+ * @param object object to check against the interface
+ * @returns boolean
+ */
+function instanceOfUser(object: any): object is IUser {
+  return (
+    'email' && 
+    'full_name' && 
+    'role' && 
+    'password' && 
+    'phone_number'  in object
+  )
+}
 // beware ts is not typesafe at runtime perfom some valiation
 
