ea2abd5071b46454c7bac922d2c8256652f7e553
KAMAH <limitlesskds@gmail.com>
2023-05-01 20:51:11 +0100

    MongoDB driver configuration setup for application

---
+++ pos/src/models/connection.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const URI = process.env.MONGO_ATLAS_URI || process.env.MONGO_DOCKER_URI
+
+/**
+ * Mongodb client used accross the application
+ */
+export const client = new MongoClient(URI as string, {
+  maxPoolSize: 100,
+  minPoolSize: 0,
+  maxIdleTimeMS: 1000,
+  socketTimeoutMS: 8000
+});
+
+
+
+client.on("connectionPoolCreated", function(pool) {
+  console.log("connection pool acquired... " + pool.options?.maxIdleTimeMS)
+})
+
+client.on("connectionPoolClosed", function(pool) {
+  console.log("Pool clossed " + pool.time)
+})
+
