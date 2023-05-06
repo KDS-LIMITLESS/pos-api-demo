9cd8af35521e5d7e4c9e241539a73f89b02aeb12
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-05-06 15:19:54 +0100

    fix: null bugs

--- pos/src/models/restaurantItems.ts
+++ pos/src/models/restaurantItems.ts
@@ -2,7 +2,6 @@ import { ItemsCollection, RestaurantItemsCollection } from './collections'
 import { InsertOneResult } from 'mongodb'
 import { RestaurantItem } from './types'
 import { IItem } from './items'
-import { ItemsModel } from './items'
 
 
 export interface RestaurantItems {
@@ -11,28 +10,46 @@ export interface RestaurantItems {
 }
 
 export class RestaurantItemsModel {
+
     /**
-        import an Item
-        @param: item_name, item_category, item_price
+        Create a new RestaurantItems 
+        @param: restaurantID
+        @returns RestaurantItems 
+    */ 
+    async createRestaurantItems(restaurantID: string): Promise<RestaurantItems> {
+        const newRestaurantItems : RestaurantItems = {
+        _id: restaurantID,
+        items: []
+        }
+        const insertResult: InsertOneResult<RestaurantItems> = await RestaurantItemsCollection.insertOne(newRestaurantItems);
+        return newRestaurantItems;
+    }
+
+    /**
+        gets an Item from the Items model
+        @param: item_name
         @returns Item
     */
-    async importItem(newItem: IItem, restaurantID:string): Promise<RestaurantItems> {
-        // const item : IItem = (new ItemsModel).getItem(newItem.item_name);
+
+    async getItemFromItems(item_name: string): Promise<IItem> {
+        const item: IItem | null = await ItemsCollection.findOne({ item_name: item_name });
+        return (item!);
+    }
 
 
-        // if (!RestaurantItemsCollection.findOne({_id: restaurantID}, {item_name: newItem.item_name}) ) {
-        const newRestaurantItems : RestaurantItems = {
-            _id: restaurantID,
-            items: [newItem]
-        }     
-        const insertResult: InsertOneResult<RestaurantItems> = await RestaurantItemsCollection.insertOne(newRestaurantItems);
-        return newRestaurantItems
-        
-
-        // await RestaurantItemsCollection.updateOne({ _id: newItem.restaurantID }, { $push: { items : item } });
-
-        // return (item);
-
+    /**
+        import an Item
+        @param: item_name, item_price, restaurantID
+        @returns Item
+    */
+    async importItem(newItem: IItem, restaurantID:string): Promise<IItem> {
+        const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne({ _id: restaurantID });
+        restaurantItems!.items.push(newItem);
+        await RestaurantItemsCollection.updateOne(
+            { _id: restaurantID },
+            { $set: { items: restaurantItems!.items } }
+        );
+        return (newItem!);
     }
 
     /**
@@ -41,9 +58,8 @@ export class RestaurantItemsModel {
         @returns Items Array
     */
 
-    async getAllItems(restaurantID: string): Promise<RestaurantItem> {
-        const restaurantItems: RestaurantItem | null = await RestaurantItemsCollection.findOne({ _id: restaurantID });
-        // const items: RestaurantItem = restaurantItems!
+    async getAllItems(restaurantID: string): Promise<RestaurantItems> {
+        const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne({ _id: restaurantID });
         return (restaurantItems!);
     }
 
@@ -53,12 +69,12 @@ export class RestaurantItemsModel {
         @returns Item 
     */
 
-    async getItem(restaurantItem: RestaurantItem): Promise<RestaurantItems | null> {
-
-        const item: RestaurantItems | null = await RestaurantItemsCollection.findOne(
-            { _id: restaurantItem.restaurantID }, {item_name: restaurantItem.item_name}
-        );
-        return (item);
+    async getItem(restaurantItem: RestaurantItem): Promise<IItem | null> {
+        const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne({ _id: restaurantItem.restaurantID });
+        const item: IItem | undefined = restaurantItems!.items.find((item) => {
+            return (item.item_name == restaurantItem.item_name);
+        });
+        return (item ? item : null);
     }
 
 
@@ -68,51 +84,47 @@ export class RestaurantItemsModel {
         @returns Item 
     */
 
-    async updateItemPrice(restaurantItem: RestaurantItem): Promise<IItem | null> {
+    async updateItemPrice(restaurantItem: RestaurantItem): Promise<IItem> {
             
             const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne(
                 { _id: restaurantItem.restaurantID }
-            );
-    
-            const item: IItem | null = restaurantItems.items.find((item) => {
+            )
+
+            const item: IItem | undefined = restaurantItems!.items.find((item) => {
                 return (item.item_name == restaurantItem.item_name);
             });
-    
-            item.item_price = restaurantItem.item_price;
-    
-            await RestaurantItemsCollection.updateOne(
-                { _id: restaurantItem.restaurantID },
-                { $set: { items: restaurantItems.items } }
-            );
-    
-            return (item);
-        }
+            if (item) {
+                item.item_price = restaurantItem.item_price;
+                await RestaurantItemsCollection.updateOne(
+                    { _id: restaurantItem.restaurantID },
+                    { $set: { items: restaurantItems!.items } }
+                );
+            }
+            return (item!);
+    }
 
     /**
         delete an item in a particular restaurant
         @param: restaurantID, item_name
-        @returns Item 
+        @returns Boolean 
     */
 
-    async deleteItem(restaurantItem: RestaurantItem): Promise<IItem | null> {
-
-        const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne(
-            { _id: restaurantItem.restaurantID }
-        );
-
-        const item: IItem | null = restaurantItems.items.find((item) => {
+    async deleteItem(restaurantItem: RestaurantItem): Promise<Boolean> {
+        const restaurantItems: RestaurantItems | null = await RestaurantItemsCollection.findOne({ _id: restaurantItem.restaurantID });
+        const item: IItem | undefined = restaurantItems!.items.find((item) => {
             return (item.item_name == restaurantItem.item_name);
         });
-
-        const deletedItem = await RestaurantItemsCollection.updateOne(
-            { _id: restaurantItem.restaurantID },
-            { $pull: { items: { item_name: restaurantItem.item_name } } }
-        );
-
-        if (deletedItem != null)
-            return (item);
-        else
-            return (null);
+        if (item) {
+            restaurantItems!.items = restaurantItems!.items.filter((item) => {
+                return (item.item_name != restaurantItem.item_name);
+            });
+            await RestaurantItemsCollection.updateOne(
+                { _id: restaurantItem.restaurantID },
+                { $set: { items: restaurantItems!.items } }
+            );
+            return (true);
+        }
+        return (false);
     }
 
 }
--- pos/src/services/restaurantItems.ts
+++ pos/src/services/restaurantItems.ts
@@ -1,5 +1,5 @@
 import { IItem } from "../models/items";
-import { RestaurantItemsModel } from "../models/restaurantItems";
+import { RestaurantItemsModel, RestaurantItems } from "../models/restaurantItems";
 import { RestaurantItem } from "../models/types";
 import { LogError } from "../utils/errors";
 import HttpStatusCodes from "../../../app-constants/HttpStatusCodes";
@@ -8,20 +8,29 @@ import AppConstants from "../../../app-constants/custom";
 const _rIM = new RestaurantItemsModel();
 
 async function importItem(newItem: RestaurantItem): Promise<IItem> {
-  const item: IItem = await _rIM.importItem(newItem);
+  let restaurantItems: RestaurantItems | null = await _rIM.getAllItems(newItem.restaurantID);
+
+  if (restaurantItems == null) {
+    restaurantItems = await _rIM.createRestaurantItems(newItem.restaurantID)
+    }
+
+  const item: IItem = await _rIM.getItemFromItems(newItem.item_name);
 
   if (item == null) {
-    throw new LogError(
-      HttpStatusCodes.INTERNAL_SERVER_ERROR,
-      AppConstants.CREATION_FAIL
-    );
+    throw new LogError(HttpStatusCodes.NOT_FOUND, AppConstants.DOES_NOT_EXIST);
   }
 
-  return item;
+  const importedItem = await _rIM.importItem(item, newItem.restaurantID);
+  return (importedItem);
+
 }
 
-async function getAllItems(restaurantID: string): Promise<IItem[]> {
-  const items: IItem[] = await _rIM.getAllItems(restaurantID);
+async function getAllItems(restaurantID: string): Promise<RestaurantItems> {
+  const items: RestaurantItems = await _rIM.getAllItems(restaurantID);
+
+  if (items == null) {
+    throw new LogError(HttpStatusCodes.NOT_FOUND, AppConstants.DOES_NOT_EXIST);
+  }
 
   return items;
 }
@@ -46,14 +55,14 @@ async function updateItemPrice(restaurantItem: RestaurantItem): Promise<IItem> {
   return item;
 }
 
-async function deleteItem(restaurantItem: RestaurantItem) {
-  const item: IItem | null = await _rIM.deleteItem(restaurantItem);
+async function deleteItem(restaurantItem: RestaurantItem): Promise<Boolean> {
+  const success: Boolean = await _rIM.deleteItem(restaurantItem);
 
-  if (item == null) {
+  if (!success) {
     throw new LogError(HttpStatusCodes.NOT_FOUND, AppConstants.DOES_NOT_EXIST);
   }
 
-  return item;
+  return success;
 }
 
 export default {
