d01c806f29e25806173856da1d5429d1a40a2cd0
KAMAH <limitlesskds@gmail.com>
2023-05-21 19:38:27 +0100

    Added async-errors package to send error responses to client

--- pos/src/utils/errors.ts
+++ pos/src/utils/errors.ts
@@ -2,14 +2,11 @@ import HttpStatusCodes from '../app-constants/HttpStatusCodes';
 
 export class LogError extends Error {
   status: HttpStatusCodes;
-  payload?: Record<string, string | number>;
 
   constructor(
     status:HttpStatusCodes, message:string , 
-    payload?: Record<string, string | number>
   ){
     super(message);
     this.status = status;
-    this.payload = payload;
   }
 }
